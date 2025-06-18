7bad0dff317af5e270a9694283b01001c4d60e15
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-18 16:04:22 +0700

    Update MaxOnes.java

--- MaxOnes.java
+++ MaxOnes.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        int max =0;
-        int count =0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 1){
+    public int findMaxConsecutiveOnes(int[] nums) {//input 1100110
+        int max =0;// this count the max consecutive of ones
+        int count =0;// this count the ones
+        for(int i=0;i<nums.length;i++){//go through the nums
+            if(nums[i] == 1){//if equal to 1 to count to 2
                 count++;
-                if(count > max){
+                if(count > max){// here put it in max so 2 in max
                     max = count;
                 }
-            }else{
+            }else{//else there is a zero is going break the streak and reset to zero
                 count =0;
             }
         }
-        return max;
+        return max;// here it return the max streak which is 2
     }
 }
